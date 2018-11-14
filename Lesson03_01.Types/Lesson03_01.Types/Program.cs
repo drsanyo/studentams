e86b045b410f05c9c272351ddd89795b308b1e66
dr.sanyo <dr.sanyo@gmail.com>
2018-11-14 20:39:21 +0200

    Added Lesson03_01.Type

---
+++ Lesson03_01.Types/Lesson03_01.Types/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_01.Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0;
+
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+            a = 254;
+
+            a++;
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
