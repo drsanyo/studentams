36b4dd174bf879ee1cf470b65be9ad7d5f65bbe1
dr.sanyo <dr.sanyo@gmail.com>
2019-05-01 13:11:29 +0300

    fixup! Merge branch 'master' of https://github.com/drsanyo/studentams

---
+++ Lesson03_06.if/Lesson03_06.if/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_06.@if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite grupes pavadinima:");
+            string grupesPavadinimas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite grupes nariu skaiciu:");
+            int nariuSkaicius = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out nariuSkaicius))
+            {
+                Console.WriteLine("Ivestas nekorektiskas skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Grupes pavadinimas yra: {0}, nariu skaicius yra {1}",
+                grupesPavadinimas, nariuSkaicius);
+            Console.ReadLine();
+        }
+    }
+}
