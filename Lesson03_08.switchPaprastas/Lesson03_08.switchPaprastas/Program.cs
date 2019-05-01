dcabb80a5f21c68c7bb6dc966080633b7b3ce43d
dr.sanyo <dr.sanyo@gmail.com>
2019-05-01 14:38:17 +0300

    paprastas switch

---
+++ Lesson03_08.switchPaprastas/Lesson03_08.switchPaprastas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_08.switchPaprastas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Koki veiksma norite atlikti?");
+            Console.WriteLine("[1] - istrinti vartotoja, [2] - sukurti vartotoja, [3] - redaguoti esama");
+
+            int veiksmas = int.Parse(Console.ReadLine());
+
+            switch (veiksmas)
+            {
+                case 1:
+                    Console.WriteLine("vartotojas sekmingai istrintas...");
+                    break;
+                case 2:
+                    Console.WriteLine("vartotojas sekmingai sukurtas...");
+                    break;
+                case 3:
+                    Console.WriteLine("Vartotojo redagavimo rezimas...");
+                    break;
+                default:
+                    Console.WriteLine("Nezinomas veiksmas. Paspauskite [ENTER] kad uzdarytymet langa.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+            }
+
+            Console.WriteLine("Paspauskite [ENTER] kad uzdarytymet langa.");
+            Console.ReadLine();
+        }
+    }
+}
