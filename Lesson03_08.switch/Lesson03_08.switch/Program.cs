b2b055fe2a8b28d32eb08337e03194b1b0f992f2
dr.sanyo <dr.sanyo@gmail.com>
2019-05-01 13:59:17 +0300

    advanced switch

---
+++ Lesson03_08.switch/Lesson03_08.switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_08.@switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite temperatura:");
+            int temperatura = int.Parse(Console.ReadLine());
+
+            switch (temperatura)
+            {
+                case int val when val < 0:
+                    Console.WriteLine("Esant normaliam slegiui vanduo tokioje temperatūroje uzsala");
+                    break;
+                case int val when (val > 0) && (val < 99) :
+                    Console.WriteLine("Esant normaliam slegiui vanduo tokioje temperatūroje neverda");
+                    break;
+                case int val when (val > 100):
+                    Console.WriteLine("Esant normaliam slegiui vanduo tokioje temperatūroje uzverda");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
