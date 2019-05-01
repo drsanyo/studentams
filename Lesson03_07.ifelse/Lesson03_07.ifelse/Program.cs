534e7f647903045fa73fb17c95e08b5971e6e7d0
dr.sanyo <dr.sanyo@gmail.com>
2019-05-01 13:32:11 +0300

    if be figuriniu skliaustu

---
+++ Lesson03_07.ifelse/Lesson03_07.ifelse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_07.ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite spalva:");
+            string spalva = Console.ReadLine();
+
+            if ((spalva == "geltona") || (spalva == "zalia") || (spalva == "raudona"))
+                Console.WriteLine("{0} - tai spalva is trispalves", spalva);
+            else
+                Console.WriteLine("{0} - spalva spalva nepriklauso trispalvei", spalva);
+
+            Console.ReadLine();
+        }
+    }
+}
