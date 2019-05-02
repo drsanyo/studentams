92485438f9a42c797239ad1adf02193802c2774f
dr.sanyo <dr.sanyo@gmail.com>
2019-05-02 12:48:49 +0300

    sukeitimas

---
+++ Lesson02_02.ndSukisti/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+
+            Console.WriteLine("a:{0}, b:{1}", a, b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("a:{0}, b:{1}", a, b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
