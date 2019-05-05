5d79a0cfb4936c4efafe55a95159802b78c8db99
dr.sanyo <dr.sanyo@gmail.com>
2019-05-05 13:11:15 +0300

    Papildytas for ciklas

--- Lesson04_01.for/Lesson04_01.for/Program.cs
+++ Lesson04_01.for/Lesson04_01.for/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lesson04_01.@for
@@ -10,10 +11,72 @@ namespace Lesson04_01.@for
     {
         static void Main(string[] args)
         {
+            // paprastas for cilkas
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
             }
+
+            // for ciklas priesinga krytimi
+            for (int i = 10-1; i >= 10; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // for ciklas su nestandartiniu zingsniu
+            for (int i = 0; i < 10; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // ... iteracijos per simbolių diapazoną.
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+
+
+            // ciklas cikle ir vardu klaidos
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("i:{0}, j:{1}", j, i);
+                }
+            }
+
+
+            // itecija per stringa
+            string vardas = "C#Kursas";
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                char current = vardas[i];
+                System.Console.WriteLine(current);
+            }
+
+
+            // progress bar
+            for (int i = 0; i < 50; i++)
+            {
+                // reikia pridėti using System.Threading;
+                Thread.Sleep(50); // milisekundes
+                System.Console.Write("#");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pabaiga");
+
+
+            // amzinasis ciklas
+
+            for (; ; )
+            {
+                Console.Write("?");
+                break;
+            }
+            Console.WriteLine("");
+
             Console.ReadLine();
         }
     }
