878721f9f10296574f3dbd662284c1f94be18c8c
dr.sanyo <dr.sanyo@gmail.com>
2019-05-13 10:41:21 +0300

    Lesson06_01.findMax

---
+++ Lesson06_01.findMax/Lesson06_01.findMax/Lesson06_01.findMax/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06_01.findMax
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 5;
+            int c = 179;
+            int max;
+            Console.WriteLine(max);
+
+            Console.ReadLine();
+        }
+    }
+}
