c7744d5a46fd4fde4e63b674eac67f966f8738dc
dr.sanyo <dr.sanyo@gmail.com>
2019-05-19 15:23:48 +0300

    Struct book

---
+++ Lesson07_02.book/Lesson07_02.book/Lesson07_02.book/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson07_02.book
+{
+    class Program
+    {
+        struct Books
+        {
+            public string title;
+            public string author;
+            public string subject;
+            public int book_id;
+        };
+
+        static void Main(string[] args)
+        {
+            Books knyga1; 
+            Books knyga2;
+        }
+    }
+}
