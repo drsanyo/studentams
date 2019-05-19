0de827e4af4edf6728fe2a6fe0f27cbd7445f36c
dr.sanyo <dr.sanyo@gmail.com>
2019-05-19 16:31:24 +0300

    Lesson07_01

---
+++ Lesson07_01.struct/Lesson07_01.struct/Lesson07_01.struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson07_01.@struct
+{
+    class Program
+    {
+
+        private static int MetinisVidurkis(int trim1, int trim2, int trim3)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int trimestras1 = 8;
+            int trimestras2 = 10;
+            int trimestras3 = 9;
+
+            Console.WriteLine(MetinisVidurkis(trimestras1, trimestras2, trimestras3));
+            Console.ReadLine();
+        }
+    }
+}
